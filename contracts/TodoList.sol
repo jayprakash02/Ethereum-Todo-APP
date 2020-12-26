0e904606de58d3612c5cf88a2a4ddfa1af2fae5c
Jay Prakash <unijay12@gmail.com>
2020-12-27 01:18:48 +0530

    completed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -14,7 +14,10 @@ contract TodoList {
     string content,
     bool completed
   );
-
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
   constructor() public{
     createTask("hello World");
   }
@@ -22,5 +25,11 @@ contract TodoList {
     taskCount++;
     tasks[taskCount]=Task(taskCount,_content,false);
     emit TaskCreated(taskCount,_content,false);
+  }
+  function toogleComplete(uint _id) public{
+    Task memory _task = tasks[_id];
+    _task.completed=!_task.completed;
+    tasks[_id]=_task;
+    emit TaskCompleted(_id,_task.completed);
   }
 }
